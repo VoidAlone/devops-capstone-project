2c13b53a7b474472042abf16434929f51fac9167
VoidAlone <glassbox93@gmail.com>
2026-03-29 01:28:22 -0400

    trying to fix formatting for flake

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -73,14 +73,8 @@ class TestAccountService(TestCase):
             accounts.append(account)
         return accounts
 
-    ######################################################################
-    #  S E C U R I T Y   T E S T   C A S E S
-    ######################################################################
-
     def test_security_headers(self):
-        """
-        It should return security headers
-        """
+        """It should return security headers"""
         response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         headers = {
@@ -98,7 +92,6 @@ class TestAccountService(TestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         # Check for the CORS header
         self.assertEqual(response.headers.get('Access-Control-Allow-Origin'), '*')
-
 
     ######################################################################
     #  A C C O U N T   T E S T   C A S E S
